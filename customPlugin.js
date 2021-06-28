86b38a16df28547ad5bffb04c8e7c2b7b504be72
Pramod Joshi <pro.joshi.droid@gmail.com>
2021-06-28 14:46:33 -0500

    first commit

---
+++ customPlugin.js
@@ -0,0 +1,13 @@
+<script>
+kWidget.embed({
+    "targetId": "kaltura_player_1382198751",
+    "wid": "_243342",
+    "uiconf_id": 20540612,
+    "flashvars": {
+        "streamerType": "auto",
+        "IframeCustomPluginCss1" : 'customSkin.css'
+    },
+    "cache_st": 1382198751,
+    "entry_id": "1_sf5ovm7u"
+});
+</script>
