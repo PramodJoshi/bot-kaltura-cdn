63d530054185c167c9edfe1834ae81d9637668a3
Pramod Joshi <pro.joshi.droid@gmail.com>
2021-06-28 14:53:15 -0500

    new changes

--- customPlugin.js
+++ customPlugin.js
@@ -5,7 +5,7 @@ kWidget.embed({
     "uiconf_id": 20540612,
     "flashvars": {
         "streamerType": "auto",
-        "IframeCustomPluginCss1" : 'customSkin.css'
+        "IframeCustomPluginCss1" : 'https://cdn.jsdelivr.net/gh/PramodJoshi/bot-kaltura-cdn/customSkin.css'
     },
     "cache_st": 1382198751,
     "entry_id": "1_sf5ovm7u"
