880f1ebb3ce35a944572d6f28b824bd4438e1974
Pramod Joshi <pro.joshi.droid@gmail.com>
2021-07-01 11:09:23 -0500

    ct logo

--- customPlugin.js
+++ customPlugin.js
@@ -3,16 +3,16 @@
 
     mw.kalturaPluginWrapper(function () {
         mw.PluginManager.add('myComponent', mw.KBaseComponent.extend({
-            // Set basic config for all components
+            // Set basic config for the component
             defaultConfig: {
                 align: "right",
                 "parent": mw.isMobileDevice() ? 'topBarContainer' : 'controlsContainer',
                 smartContainer: 'morePlugins',
                 smartContainerCloseEvent: 'ctPlayer',
                 displayImportance: "low",
-                showTooltip: false,
                 title: 'ClassTranscribe Player',
-                order: 41
+                order: 31,
+                img: 'https://uofi.box.com/shared/static/1g67u1c8atarjmcbex1qpjeb763d94dj.png'
             },
             setup: function () {
                 var _this = this;
@@ -23,17 +23,28 @@
             ctPlayer: function () {
                 var ctPlayerUrl = 'https://classtranscribe-dev.ncsa.illinois.edu/liveplayer?videosrc=https://cdnapisec.kaltura.com/p/';
                 ctPlayerUrl += this.getPlayer().kpartnerid + '/sp/0/playManifest/entryId/';
-                ctPlayerUrl += this.getPlayer().kentryid + '/format/applehttp/protocol/https/flavorParamId/manifest.m3u8';
-
+                ctPlayerUrl += this.getPlayer().kentryid + '/format/applehttp/protocol/https/flavorParamId/ manifest.m3u8';
+                
                 window.open(encodeURI(ctPlayerUrl));
             },
             getComponent: function () {
                 var _this = this;
+                var $img = $('<img />')
+                    .attr({
+                        alt: this.getConfig('title'),
+                        src: this.getConfig('img')
+                    });
+    
                 if (!this.$el) {
-                    this.$el = $('<button />')
-                        .attr('title', this.getConfig('title'))
-                        .addClass("btn icon-play" + this.getCssClass())
-                        .click(function () {
+                    this.$el = $('<div />')
+                        .addClass(this.getCssClass())
+                        .append(
+                            $('<a />')
+                                .attr({
+                                    'title': this.getConfig('title'),
+    
+                                }).append($img)
+                        ).click(function () {
                             if (_this.isDisabled) return;
                             _this.getPlayer().triggerHelper('ctPlayer');
                         });
